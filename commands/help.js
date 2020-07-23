41f96f5956e4095e8430e959525b1926263c5945
Neel <neelmedhanshgopaul@gmail.com>
2020-07-23 22:36:58 +0400

    Update usage

--- commands/help.js
+++ commands/help.js
@@ -12,14 +12,16 @@ module.exports = {
         { name: '`.about`', value: 'Provides a description and some stats of the bot.' },
         { name: '`.demo`', value: 'Provides a demo video of the bot.' },
         { name: '`.ping`', value: 'Returns the bot\'s latency and API ping.' },
-        { name: '`.kick`', value: 'Kicks a member from the current server.\n Usage: `.kick @username`' },
-        { name: '`.ban`', value: 'Bans a member from the current server.\n Usage: `.ban @username`' },
+        { name: '`.kick`', value: 'Kicks a member from the current server.\n Usage: `.kick @username/member_id`' },
+        { name: '`.ban`', value: 'Bans a member from the current server.\n Usage: `.ban @username/member_id`' },
         { name: '`.unban`', value: 'Unbans a member from the current server.\n Usage: `.unban user_id`' },
-        { name: '`.mute`', value: 'Prevents a member from sending messages/adding reactions.\n Usage: `.mute @username`' },
-        { name: '`.unmute`', value: 'Gives a member back the permissions to send messages/add reactions.\n Usage: `.unmute @username`' },
+        { name: '`.mute`', value: 'Prevents a member from sending messages/adding reactions.\n Usage: `.mute @username/member_id`' },
+        { name: '`.unmute`', value: 'Gives a member back the permissions to send messages/add reactions.\n Usage: `.unmute @username/member_id`' },
         { name: '`.purge`', value: 'Deletes the number of messages provided.\n Usage: `.purge [number_of_messages_to_delete]`' },
-        { name: '`.add-admin`', value: 'Adds the "Admin" role to a member.\n Usage: `.add-admin @username`' },
-        { name: '`.remove-admin`', value: 'Removes the "Admin" role from a member.\n Usage: `.remove-admin @username`' },
+        { name: '`.add-role`', value: 'Adds the specified role to a member.\n Usage: `.add-role @username/member_id <role-name>`' },
+        { name: '`.remove-role`', value: 'Removes the specified role from a member.\n Usage: `.remove-role @username/member_id <role-name>`' },
+        { name: '`.add-admin`', value: 'Adds the "Admin" role to a member.\n Usage: `.add-admin @username/member_id`' },
+        { name: '`.remove-admin`', value: 'Removes the "Admin" role from a member.\n Usage: `.remove-admin @username/member_id`' },
       )
       .setColor('#63D6FF');
 
